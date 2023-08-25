8d4d37c7b2242cfcf52f1ff22806413e6aafc8ac
Konstantin Deev <kn.deev@gmail.com>
2023-08-25 18:10:41 -0500

    downgrade pydantic

--- pyonwater/models/models.py
+++ pyonwater/models/models.py
@@ -2,16 +2,13 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-from pydantic import BaseModel, Field
-
-if TYPE_CHECKING:
-    from datetime import datetime
+from dataclasses import dataclass
+from datetime import datetime
 
 
-class DataPoint(BaseModel):
+@dataclass
+class DataPoint:
     """One data point representation."""
 
-    dt: datetime = Field(..., description="datetime with timezone")
-    reading: float = Field(..., description="reading in m^2 or gallons")
+    dt: datetime
+    reading: float
